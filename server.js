05aac3b42363791d06f1c375bfc709aa846ff0bd
MKA <matrixkali@wp.pl>
2019-01-19 12:58:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -56,8 +56,8 @@ app.post("/api/posts", function(req, res) {
   var newContact = req.body;
   newContact.createDate = new Date();
 
-  if (!req.body.name) {
-    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  if (!req.body.title) {
+    handleError(res, "Invalid user input", "Must provide a title.", 400);
   } else {
     db.collection(POSTS_COLLECTION).insertOne(newContact, function(err, doc) {
       if (err) {
