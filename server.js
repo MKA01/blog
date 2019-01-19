e6e6a6ae2d850a920ef3ebf341640c2e52fdb860
MKA <matrixkali@wp.pl>
2019-01-19 13:05:58 +0100

    Deploy ready

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var POSTS_COLLECTION = "posts";
 var app = express();
 app.use(bodyParser.json());
 
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
@@ -42,8 +46,8 @@ function handleError(res, reason, message, code) {
  *    POST: creates a new post
  */
 
-app.get("/api/posts", function(req, res) {
-  db.collection(POSTS_COLLECTION).find({}).toArray(function(err, docs) {
+app.get("/api/posts", function (req, res) {
+  db.collection(POSTS_COLLECTION).find({}).toArray(function (err, docs) {
     if (err) {
       handleError(res, err.message, "Failed to get posts.");
     } else {
@@ -52,14 +56,14 @@ app.get("/api/posts", function(req, res) {
   });
 });
 
-app.post("/api/posts", function(req, res) {
+app.post("/api/posts", function (req, res) {
   var newContact = req.body;
   newContact.createDate = new Date();
 
   if (!req.body.title) {
     handleError(res, "Invalid user input", "Must provide a title.", 400);
   } else {
-    db.collection(POSTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    db.collection(POSTS_COLLECTION).insertOne(newContact, function (err, doc) {
       if (err) {
         handleError(res, err.message, "Failed to create new post.");
       } else {
@@ -75,8 +79,8 @@ app.post("/api/posts", function(req, res) {
  *    DELETE: deletes post by id
  */
 
-app.get("/api/posts/:id", function(req, res) {
-  db.collection(POSTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+app.get("/api/posts/:id", function (req, res) {
+  db.collection(POSTS_COLLECTION).findOne({_id: new ObjectID(req.params.id)}, function (err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to get post");
     } else {
@@ -85,11 +89,11 @@ app.get("/api/posts/:id", function(req, res) {
   });
 });
 
-app.put("/api/posts/:id", function(req, res) {
+app.put("/api/posts/:id", function (req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(POSTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+  db.collection(POSTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function (err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to update post");
     } else {
@@ -99,8 +103,8 @@ app.put("/api/posts/:id", function(req, res) {
   });
 });
 
-app.delete("/api/posts/:id", function(req, res) {
-  db.collection(POSTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+app.delete("/api/posts/:id", function (req, res) {
+  db.collection(POSTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function (err, result) {
     if (err) {
       handleError(res, err.message, "Failed to delete post");
     } else {
