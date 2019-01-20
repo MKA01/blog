2bed24be6e60de91ffc869b26c178fde2ec0ed04
MKA <matrixkali@wp.pl>
2019-01-20 17:58:49 +0100

    Routing fix

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,28 +4,37 @@ import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { AppComponent } from './app.component';
 
 export const AppRoutes: Routes = [
-  {
-    path : '',
-    component : LoginPageComponent
-  },
-  {
-    path : 'login',
-    component : LoginPageComponent
-  },
-  {
-    path : 'home',
-    component : HomeComponent
-  },
-  {
-    path : '**',
-    redirectTo : '/home'
-  },
-  {
-    path : 'register',
-    component : RegisterPageComponent
-  }
-];
+    {
+      path : 'app',
+      component : AppComponent,
+      children : [
+        {
+          path : 'login',
+          component : LoginPageComponent
+        },
+        {
+          path : 'home',
+          component : HomeComponent
+        },
+        {
+          path : 'register',
+          component : RegisterPageComponent
+        }
+      ]
+    },
+    {
+      path : '**',
+      redirectTo : '/home'
+    },
+    {
+      path : '',
+      redirectTo : 'app/home',
+      pathMatch : 'full'
+    }
+  ]
+;
 
 export const AppRouting: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
