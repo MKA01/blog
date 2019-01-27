a0f07fd0292dbc03ef236c31e794664ab095e9ba
MKA <matrixkali@wp.pl>
2019-01-27 10:22:43 +0100

    Code reformat

--- src/app/components/contact-me/contact-me.component.spec.ts
+++ src/app/components/contact-me/contact-me.component.spec.ts
@@ -8,9 +8,9 @@ describe('ContactMeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactMeComponent ]
+      declarations : [ ContactMeComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
