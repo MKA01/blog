4a83f0a9317a392104cac1ab2a12c265e6603cf7
MKA <matrixkali@wp.pl>
2019-01-28 19:21:01 +0100

    Update add-comment.component.ts

--- src/app/components/add-comment/add-comment.component.ts
+++ src/app/components/add-comment/add-comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostComment } from '../../models/post-comment';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PostService } from '../../services/post.service';
